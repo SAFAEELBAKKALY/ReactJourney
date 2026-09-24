9900a80eaeda60e877e32ce146e061b5c24672bd
SAFAEELBAKKALY <125454375+SAFAEELBAKKALY@users.noreply.github.com>
2026-09-25 00:06:13 +0100

    Correction de syntaxe

--- mini-projects/digital-business-card/src/Header.jsx
+++ mini-projects/digital-business-card/src/Header.jsx
@@ -5,7 +5,7 @@ export default function Header()
             <img className="header-img" src='./pic/laure.png' />
             <h2 className='user-name'>Laura Smith</h2>
             <h3 className='user-job'>Frontend Developer</h3>
-            <a className='user-site' href="###">laurasmith.website</a>
+            <a className='user-site'>laurasmith.website</a>
             <div className="btns">
                 <button className="email-btn"><img src='./pic/email.png'/>Email</button>
                 <button className="linkedin-btn"><img src='./pic/linkedin.png'/>Linkedin</button>
